b394c8de4a7d9f6d170548b61ae4f124ba9d64c5
james <19514687+jrondina@users.noreply.github.com>
2019-07-25 14:28:17 -0400

    primitive types

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -8,7 +8,14 @@
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
-// fn ..... {
+fn calculate_price(apples: u32) -> u32 {
+    if apples > 40 {
+        return apples;
+    }
+    else {
+        return 2 * apples;
+    }
+}
 
 // Don't modify this function!
 #[test]
